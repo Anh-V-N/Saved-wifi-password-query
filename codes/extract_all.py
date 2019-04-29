e272eb58e5f405cf9252c5edb7393d3bd8a7b1fd
Anh-V-N <vuanh0110@gmail.com>
2019-04-29 02:34:02 -0400

    New version. Output is in table format

--- codes/extract_all.py
+++ codes/extract_all.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+
 '''
 This script will extract all saved wifi passwords on a computer.
 '''
@@ -9,29 +10,47 @@ if 'win' in sys.platform:  # Windows platform
     enum = subprocess.check_output(enum)
     enum = str(enum).split("\\r\\n")
     # Make a list of saved SSIDs
-    profile_list = []
+    saved_ssids = []
+
     for line in enum:
         if 'All User Profile' in line:
             ssid = line.split(':')[1].strip()
-            profile_list.append(ssid)
-    # Extract the passwords
-    for wifi_ssid in profile_list:
+            saved_ssids.append(ssid)
+    # Make a list of passwords
+    passwords_list = []
+    for wifi_ssid in saved_ssids:
         query = 'netsh wlan show profiles name=%s key=clear' % (wifi_ssid)
         output = subprocess.check_output(query)
-        output = str(output) .split("\\r\\n")
-        for line in output:
-            if 'Key Content' in line:
-                password = line.split(':')[1].strip()
-                print("Password for SSID %s is %s" % (wifi_ssid, password))
-    input('Press enter to exit...')
+        if 'Key Content' in str(output):
+            output = str(output) .split("\\r\\n")
+            for line in output:
+                if 'Key Content' in line:
+                    password = line.split(':')[1].strip()
+                    passwords_list.append(password)
+                    # print("Password for SSID %s is %s" % (wifi_ssid, password))
+        else:
+            passwords_list.append("\033[31mNOT FOUND \033[m")  # Change text color to red
 
 elif 'linux' in sys.platform:   # Linux platform
     os.chdir('/etc/NetworkManager/system-connections/')  # Move to folder
     saved_ssids = os.listdir()  # List of saved SSIDs
+    # Make a list of passwords
+    passwords_list = []
     for item in saved_ssids:
         with open(item) as file:
-            content = file.readlines()
-            for item in content:
-                if 'psk' in item:
-                    password = item.split('=')[1]
-                print("Password for SSID %s is %s" % (wifi_ssid, password))
+            if 'psk' in file.read():
+                content = file.readlines()
+                for item in content:
+                    if 'psk' in item:
+                        password = item.split('=')[1]
+                        passwords_list.append(password)
+                    # print("Password for SSID %s is %s" % (wifi_ssid, password))
+            else:
+                passwords_list.append("\033[31mNOT FOUND \033[m")  # Change text color to red
+
+# Present the result:
+wifi_pw = dict(zip(saved_ssids, passwords_list))
+print('SSID'.ljust(15), '|'.ljust(1), 'Password')
+print('-'*30)
+for item in wifi_pw.items():
+    print(item[0].ljust(15), '|'.ljust(1), item[1])
